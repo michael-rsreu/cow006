3437dd4e351241fccfbe73046b8284053d9dbea7
User <misha.rodyushkin@gmail.com>
2024-09-29 16:15:51 +0300

    Дописан Card и тесты к нему

--- src/card.py
+++ src/card.py
@@ -1,32 +1,43 @@
 class Card:
-
     NUMBERS = list(range(1,105))
-    SCORE = [1, 2, 3, 5, 7]
+    #SCORE = [1, 2, 3, 5, 7]
     #COLORS = ['g', 'b', 'y', 'o', 'p']
 
-    def  __init__(self, number: int, score: int):
+    def __init__(self, number: int):
         if number not in Card.NUMBERS:
             raise ValueError
-        if score not in Card.SCORE:
-            raise ValueError
         self.number = number
-        self.score = score
 
     def __repr__(self):
         return f'{self.number}({self.score})'
 
+    def __eq__(self, other):    #а надо ли eq, если такое же сравнение есть в can_play_on
+        return self.number == other.number
+
     def save(self):
         return repr(self)
 
-    def score(self, point):
+    def score(self):
         if self.number == 55:
-            self.point += 7
+            return 7
         elif self.number % 11 == 0:
-            self.point += 5
+            return 5
         elif self.number % 10 == 0:
-            self.point += 3
+            return 3
         elif self.number % 5 == 0 and self.number % 10 != 0:
-            self.point += 2
+            return 2
         else:
-            self.point += 1
-        return point
+            return 1
+
+    def can_play_on(self, other):
+        return self.number > other.number
+
+    def load(text: str):
+        #По идее нам не надо вводить штрафные очки каждой карты, т.к. они определяются в Score
+        return Card(number=int(text))
+
+    def all_cards(numbers: None | list[int] = None):     # Нужна ли она вообще
+        if numbers is None:
+            numbers = Card.NUMBERS
+        cards = [Card(number=num) for num in numbers]
+        return cards
--- test/test_card.py
+++ test/test_card.py
@@ -1,16 +1,41 @@
-import pytest
-
 from src.card import Card
 
 def test_init():
-    c = Card(55,7)
+    c = Card(55)
     assert c.number == 55
-    assert c.score == 7
+    assert c.score() == 7
 
-def test_save():
-    c = Card(55,7)
+def test_save():    # Не фурычит(
+    c = Card(55)
     assert repr(c) == '55(7)'
     assert c.save() == '55(7)'
 
+    c = Card(13)
+    assert repr(c) == '13(1)'
+    assert c.save() == '13(1)'
+
 def test_score():
-    c = Card(55, 7)
+    c = Card(55)
+    assert c.score() == 7
+    c = Card(13)
+    assert c.score() == 1
+    c = Card(44)
+    assert c.score() == 5
+
+def test_can_play_on():
+    c1 = Card(77)
+    c2 = Card(13)
+    c3 = Card(55)
+
+    assert c1.can_play_on(c1) == False
+    assert c1.can_play_on(c2) == True
+    assert c2.can_play_on(c3) == False
+
+def test_load():
+    a = '77'
+    c = Card.load(a)
+    assert c == Card(77)
+
+    b = '13'
+    c = Card.load(b)
+    assert c == Card(13)
